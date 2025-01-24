884ad8f54a075fa89a0922803c136de41632aaac
KingShivamX <shivamhippalgave@gmail.com>
2025-01-24 20:50:49 +0530

    well

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3000
 
 // Middleware
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 // Routes
-app.get('/', (req, res) => {
-  res.send('Hello Jiya');
-});
+app.get("/", (req, res) => {
+    res.send("backend")
+})
 
 // Start server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+    console.log(`Server is running on port ${port}`)
+})
